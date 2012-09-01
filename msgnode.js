614ad1a82164085ca7e18ac7cee6c0cd6c18b513
lesh <lesh@sysphere.org>
2012-09-01 20:31:46 +0200

    borderguard model, working on msgnode

--- msgnode.js
+++ msgnode.js
@@ -21,6 +21,18 @@ var MsgSubscriptionMan = SubscriptionMan.extend4000({
 })
 
 
+// backbone model that uses validator on its own attributes (for initialization)
+// feed call in this case should BLOCK. at least on the level of this object's init.. 
+// we don't want other subclassed initialize functions to be called until verification is complete
+var ValidatedObject = Backbone.Model.extend4000({
+    initialize: function () {
+        var validator 
+        if (validator = this.get('validator')) {
+            Validator(validator).feed(this.attributes,function (err,data) { if (err) { throw err } })
+        }
+    }
+})
+
 var MsgNode = Backbone.Model.extend4000(
     graph.GraphNode,
     MsgSubscriptionMan,
@@ -29,7 +41,28 @@ var MsgNode = Backbone.Model.extend4000(
             
             
         }
+        
     })
 
+
+var x = new MsgNode()
+
+
+// sets realm for a message to be this.attributes.realm, 
+// passes it to other potential local subscribers and broadcasts message to other nodes connected to this node
+var BorderGuard = Backbone.Model.extend4000(
+    MsgNode,
+    ValidatedObject,
+    {        
+        validator: Validator({ realm: "String" }),
+
+        initialize: function () {
+            var self = this
+            self.subscribe({ realm: Validator().Set(self.get('realm')) },function (msg,next,pass) {
+                pass()
+                next()
+            })
+        }
+    })
 
 
