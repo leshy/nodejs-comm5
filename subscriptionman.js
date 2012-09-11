96b819d78359ae5a5263a878e95c2811ba8ec90f
lesh <lesh@sysphere.org>
2012-09-12 01:17:46 +0200

    subscriptionman with streams started

---
+++ subscriptionman.js
@@ -0,0 +1,8 @@
+var SubscriptionMan = require('subscriptionman').SubscriptionMan
+var Stream = require('stream')
+var _ = require('underscore')
+
+exports.StreamingSubscriptionMan = SubscriptionMan.extend4000({
+    
+    
+})
