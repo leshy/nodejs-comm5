154b72a8b6d207d9fa3cf2f6430f15f211eb86f7
lesh <lesh@sysphere.org>
2012-09-22 18:44:21 +0200

    http

---
+++ http/index.js
@@ -0,0 +1,51 @@
+var _ = require('underscore')
+var Backbone = require('backbone4000');
+var decorators = require('decorators'); var decorate = decorators.decorate;
+var helpers = require('helpers')
+
+var v = require('validator'); var Validator = v.Validator; var Select = v.Select
+
+var Msg = exports.Msg = require('../core/msg').Msg
+var MsgNode = exports.MsgNode = require('../core/msgnode').MsgNode
+
+
+
+// sets realm for a message to be this.attributes.realm, 
+// passes it to other potential local subscribers and broadcasts message to other nodes connected to this node
+var HttpServer = exports.HttpServer = Backbone.Model.extend4000(
+    MsgNode,
+    v.ValidatedModel,
+    {        
+        validator: Validator({ express: "Object", root: Validator().Default("/") }),
+
+        initialize: function () {
+            var self = this
+            
+            var app = this.get('express')
+
+            function requestToMsg(req,res,next) {
+                var from = req.socket.remoteAddress
+                if (from == "127.0.0.1") { if (req.headers['x-forwarded-for']) { from = req.headers['x-forwarded-for'] }}
+
+                var responseStream = self.msg({http: req.method, headers: req.headers, url: req.url, from: from})
+                
+                responseStream.read(function (msg) {
+                    console.log('msg',msg)
+                    if (!msg) { res.end() } else {
+                        res.write(JSON.stringify({headers: msg.headers, url: msg.url, method: msg.method}))
+                    }
+                })
+            }
+
+            app.get  ('*', function () { requestToMsg.apply(this,arguments) })
+            app.post ('*', function () { requestToMsg.apply(this,arguments) })
+
+            this.subscribe(true,function (msg,reply,next,transmit) { reply.end(); transmit(); })
+
+            this.logmsg(true,['http','request'],'http')
+        }
+        
+    })
+
+
+
